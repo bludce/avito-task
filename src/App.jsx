a2ed0897d4a369e6c247ffee8c83c0e7957193e6
Aleksey Zadorozhny <alekszadorozhny90@gmail.com>
2020-06-11 12:51:57 +0300

    feat: add list repo

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,33 @@ import './index.sass';
 
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import RepoList from './components/RepoList/RepoList'
+
 
 class App extends Component {
 
-  componentDidMount = () => {
-    
+  state = {
+    repositories: {}
+  }
+
+  componentDidMount = async () => {
+      let response = await fetch('https://api.github.com/search/repositories?q=stars&per_page=10&page=2&sort=stars&order=desc');
+      let repositories = await response.json();
+      this.setState({
+        repositories
+      })
+  
   }
 
   render() {
+
+    const { repositories } = this.state;
     
     return (
       <div className="app">
         <Header />
         <div className="container">
-          {/* <List /> */}
+          <RepoList repositories={repositories} />
         </div>
         <Footer />
       </div>
---
+++ src/components/RepoList/RepoList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './RepoList.sass'
+
+import RepoItem from '../RepoItem/RepoItem'
+
+const List = ({ repositories }) => {
+  
+  const {items = []} = repositories
+
+  console.log(items)
+
+  const repoItems = items.map((repo) => {
+    return (
+      <RepoItem
+        key={repo.id}
+        id={repo.id}
+        name={repo.name}
+        stars={repo.stargazers_count}
+        update={repo.updated_at}
+        language={repo.language}
+        url={repo.url}
+      />
+    )
+  });
+
+  return (
+    <div className="repo-list">{repoItems}</div>
+  );
+
+}
+
+export default List;
